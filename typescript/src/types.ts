a54f28c2f270e66f16c420ccb37c8f6c3d12fd2f
Rescind <peter.t.nguyen.98@gmail.com>
2021-04-25 15:52:26 +1000

    adjust type

--- typescript/src/types.ts
+++ typescript/src/types.ts
@@ -1,4 +1,9 @@
-export interface Coordinates {
+export class Coordinates {
     readonly x: number;
     readonly y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
 }
